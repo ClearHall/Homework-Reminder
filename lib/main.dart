af60790478ada72051aa16fdcbce975e247f1de2
Hunter Han <hunter602353@yahoo.com>
2019-08-12 01:03:49 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: PeriodsTable(title: 'Homework Reminder'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class PeriodsTable extends StatefulWidget {
+  PeriodsTable({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _PeriodsTableState createState() => _PeriodsTableState();
+}
+
+class _PeriodsTableState extends State<PeriodsTable> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  margin: new EdgeInsets.symmetric(
+                      horizontal: 10.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                    border: new Border.all(color: Colors.black),
+                    borderRadius: new BorderRadius.all(Radius.circular(5.0)),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 10.0),
+                        child: Text(
+                          "TEST",
+                          style: TextStyle(fontSize: 10),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ]),
+        ));
+  }
+}
